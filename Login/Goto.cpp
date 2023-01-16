cac1c672828ffd2639be167de7a5aa3cbdffa7ba
Ruzzel Dean Parungao <111668375+RuzzelDean@users.noreply.github.com>
2023-01-16 14:40:13 +0800

    Add files via upload

---
+++ Login/Goto.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string username = "", password = "";
+	int attempts = 0;
+
+	login:
+	system("cls");
+	
+	cout << "Username: ";
+	cin >> username;
+	
+	cout << "Password: ";
+	cin >> password;
+	
+	attempts++;
+	
+	if (username=="admin" && password=="xyz") {
+		cout << "\nHenlo Admin\n";
+
+		return 0;
+	}
+	
+	else if (attempts < 3) {
+		cout << "\nIncorrect Username/Password";
+
+		if (attempts == 1) {
+			cout << "\nTwo remaining attempts\n\n";
+		}
+		
+		else {
+			cout << "\nOne remaining attempt\n\n";
+		}
+		
+		system("pause");
+		goto login;
+	}
+	
+	else {
+		cout <<"\nIncorrect Username/Password";
+		cout <<"\nMaximum limit of attempts reached\n";
+	}
+	
+	return 0;
+}
---
+++ Login/While.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string username = "", password = "";
+	int attempts = 0;
+	
+	while (attempts < 3) {
+		system("cls");
+		
+		cout << "Username: ";
+		cin >> username;
+		
+		cout << "Password: ";
+		cin >> password;
+		
+		attempts++;
+		
+		if (username=="admin" && password=="xyz") {
+			cout << "\nHenlo Admin\n";
+			return 0;
+		}
+		
+		else if (attempts < 3) {
+			cout << "\nIncorrect Username/Password";
+			
+			if (attempts == 1) {
+				cout << "\nTwo remaining attempts\n\n";
+			}
+			
+			else {
+				cout << "\nOne remaining attempt\n\n";
+			}
+			
+			system("pause");
+		}
+		
+		else {
+			cout <<"\nIncorrect Username/Password";
+			cout <<"\nMaximum limit of attempts reached\n";
+			
+			return 0;
+		}
+	}
+	
+	return 0;
+}
